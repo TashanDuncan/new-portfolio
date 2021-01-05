ae2dc56ecfac6fc36f4eefd0494d6f3877848ea6
viewtifult <tashanduncan1994@gmail.com>
2021-01-05 21:26:53 +0000

    move sass files to app

---
+++ my-app/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = (props) => {
+    return (
+        <header className="header text-center">	    
+            This is a test
+	    </header>
+
+    );
+}
+
+export default Header;
