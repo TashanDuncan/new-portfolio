7e82edb6bac6450608e8e6c911952816cd26b400
viewtifult <tashanduncan1994@gmail.com>
2021-01-09 19:08:10 +0000

    add navigation

--- my-app/src/components/Header/Navigation.js
+++ my-app/src/components/Header/Navigation.js
@@ -1,1 +1,13 @@
 import React from 'react';
+
+const Navigation = (props) => {
+    return (
+        <header className="header text-center">	    
+           	<div className="force-overflow">
+			<h1 className="blog-name pt-lg-4 mb-0">Tashan Duncan</h1>
+            <Navbar />
+            </div>
+	    </header>
+
+    );
+}
