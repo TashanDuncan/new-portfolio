2a2b2422b825e57bae2ae8087bf7d9ae6d1c8730
viewtifult <tashanduncan1994@gmail.com>
2021-01-10 15:17:18 +0000

    add fontawesome to project

--- my-app/src/components/Header/Socials.js
+++ my-app/src/components/Header/Socials.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTwitter } from '@fortawesome/free-brands-svg-icons'
 
 const Socials = () => {
     return(
         <ul className="social-list list-inline py-2 mx-auto">
+            <li className="list-inline-item"><a><FontAwesomeIcon icon={faTwitter} fixedWidth /></a></li>
             <li className="list-inline-item"><a><i className="fab fa-twitter fa-fw"></i></a></li>
             <li className="list-inline-item"><a><i className="fab fa-linkedin-in fa-fw"></i></a></li>
             <li className="list-inline-item"><a><i className="fab fa-github-alt fa-fw"></i></a></li>
