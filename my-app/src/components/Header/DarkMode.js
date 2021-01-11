ec584095c150674dbbce54eb4ae7e19f2f6121da
viewtifult <tashanduncan1994@gmail.com>
2021-01-11 12:55:00 +0000

    change dark mode to class component to handle state

--- my-app/src/components/Header/DarkMode.js
+++ my-app/src/components/Header/DarkMode.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faAdjust } from '@fortawesome/free-solid-svg-icons'
 
-const DarkMode = () => {
+class DarkMode extends Component {
+    state = {
+        isdark: false
+    }
+    
+    render(){
     return(
-
         <div class="dark-mode-toggle text-center w-100">
         <hr class="mb-4"/>
         <h4 class="toggle-name mb-3 "><FontAwesomeIcon icon={faAdjust}  className="mr-1"/>Dark Mode</h4>
@@ -12,7 +16,7 @@ const DarkMode = () => {
         <label class="toggle-btn mx-auto mb-0" for="darkmode"></label>
         </div>
         
-    )
+    )}
 }
 
 export default DarkMode;
