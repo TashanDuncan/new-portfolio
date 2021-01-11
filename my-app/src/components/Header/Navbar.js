612ec8ea4425dc4e9be871b0383a3e24fce58d78
viewtifult <tashanduncan1994@gmail.com>
2021-01-11 17:52:06 +0000

    complete collapse button on small screens

--- my-app/src/components/Header/Navbar.js
+++ my-app/src/components/Header/Navbar.js
@@ -6,9 +6,25 @@ class Navbar extends Component {
         isExpanded: false
     }
 
+    showNavMenu = () => {
+        const nav = document.getElementById('navigation');
+        if (this.state.isExpanded){
+            this.setState({
+                isExpanded: false
+            })
+            nav.classList.remove('show')
+        } else if (this.state.isExpanded === false){
+            this.setState({
+                isExpanded: true
+            })
+            nav.classList.add('show')
+        }
+
+    }
+
     render(){
     return (
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation" onClick={this.showNavMenu}>
         <span className="navbar-toggler-icon"></span>
         </button>
 
