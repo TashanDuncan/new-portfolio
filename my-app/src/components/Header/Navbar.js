d72b0da87e0d4ff4874ad0c59c5dd50d924977d6
viewtifult <tashanduncan1994@gmail.com>
2021-01-11 17:34:07 +0000

    change navbar button to stateful class component

--- my-app/src/components/Header/Navbar.js
+++ my-app/src/components/Header/Navbar.js
@@ -1,12 +1,19 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const Navbar = () => {
+
+class Navbar extends Component {
+    state = {
+        isExpanded: false
+    }
+
+    render(){
     return (
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
         </button>
 
     );
+    }
 }
 
 export default Navbar;
