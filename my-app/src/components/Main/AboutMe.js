6a1538460bb2b2dd33d67480268ee434e5da2176
viewtifult <tashanduncan1994@gmail.com>
2021-01-15 18:24:46 +0000

    add profile photo to page

--- my-app/src/components/Main/AboutMe.js
+++ my-app/src/components/Main/AboutMe.js
@@ -1,21 +1,22 @@
 import React from 'react'
+import Image from '../../images/profile-lg.jpg'
 
 const AboutMe = () =>  (
-    <section class="about-me-section p-3 p-lg-5 theme-bg-light">
-    <div class="container">
-        <div class="profile-teaser media flex-column flex-lg-row">
+    <section className="about-me-section p-3 p-lg-5 theme-bg-light">
+    <div className="container">
+        <div className="profile-teaser media flex-column flex-lg-row">
             
-            <div class="media-body">
-                <h2 class="name font-weight-bold mb-1">Tashan Duncan</h2>
-                <div class="tagline mb-3">Web Developer</div>
-                <div class="bio mb-4">I'm a software engineer specialised in frontend and backend development for complex scalable web apps. I write about software development on <a class="link-on-bg" href="blog-home.html">my blog</a>. Want to know how I may help your project? Check out my project <a class="link-on-bg" href="projects.html">portfolio</a> and <a class="link-on-bg" href="resume.html">online resume</a>.
+            <div className="media-body">
+                <h2 className="name font-weight-bold mb-1">Tashan Duncan</h2>
+                <div className="tagline mb-3">Web Developer</div>
+                <div className="bio mb-4">I'm a software engineer specialised in frontend and backend development for complex scalable web apps. I write about software development on <a className="link-on-bg" href="blog-home.html">my blog</a>. Want to know how I may help your project? Check out my project <a className="link-on-bg" href="projects.html">portfolio</a> and <a className="link-on-bg" href="resume.html">online resume</a>.
                 </div>
-                <div class="mb-4">
-                    <a class="btn btn-primary mr-2 mb-3" href="portfolio.html"><i class="fas fa-arrow-alt-circle-right mr-2"></i><span class="d-none d-md-inline">View</span> Portfolio</a>
-                    <a class="btn btn-secondary mb-3" href="resume.html"><i class="fas fa-file-alt mr-2"></i><span class="d-none d-md-inline">View</span> Resume</a>
+                <div className="mb-4">
+                    <a className="btn btn-primary mr-2 mb-3" href="portfolio.html"><i className="fas fa-arrow-alt-circle-right mr-2"></i><span className="d-none d-md-inline">View</span> Portfolio</a>
+                    <a className="btn btn-secondary mb-3" href="resume.html"><i className="fas fa-file-alt mr-2"></i><span className="d-none d-md-inline">View</span> Resume</a>
                 </div>
             </div>
-            <img class="profile-image mb-3 mb-lg-0 ml-lg-5 mr-md-0" src="assets/images/profile-lg.jpg" alt=""/>
+            <img className="profile-image mb-3 mb-lg-0 ml-lg-5 mr-md-0" src={Image} alt=""/>
         </div>
     </div>
 </section>
