ae02b2e5071b3e794b50b64df1fd12b2cab719e2
viewtifult <tashanduncan1994@gmail.com>
2021-01-17 15:57:24 +0000

    add icons for about me section

--- my-app/src/components/Main/AboutMe.js
+++ my-app/src/components/Main/AboutMe.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import Image from '../../images/profile-lg.jpg'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowAltCircleRight, faFileAlt} from '@fortawesome/free-solid-svg-icons'
 
 const AboutMe = () =>  (
     <section className="about-me-section p-3 p-lg-5 theme-bg-light">
@@ -12,11 +14,11 @@ const AboutMe = () =>  (
                 <div className="bio mb-4">I'm a software engineer specialised in frontend and backend development for complex scalable web apps. I write about software development on <a className="link-on-bg" href="blog-home.html">my blog</a>. Want to know how I may help your project? Check out my project <a className="link-on-bg" href="projects.html">portfolio</a> and <a className="link-on-bg" href="resume.html">online resume</a>.
                 </div>
                 <div className="mb-4">
-                    <a className="btn btn-primary mr-2 mb-3" href="portfolio.html"><i className="fas fa-arrow-alt-circle-right mr-2"></i><span className="d-none d-md-inline">View</span> Portfolio</a>
-                    <a className="btn btn-secondary mb-3" href="resume.html"><i className="fas fa-file-alt mr-2"></i><span className="d-none d-md-inline">View</span> Resume</a>
+                    <a className="btn btn-primary mr-2 mb-3" href="portfolio.html"><FontAwesomeIcon icon={faArrowAltCircleRight} className="mr-2"/><span className="d-none d-md-inline">View</span> Portfolio</a>
+                    <a className="btn btn-secondary mb-3" href="resume.html"><FontAwesomeIcon icon={faFileAlt} className="mr-2"/><span className="d-none d-md-inline">View</span> Resume</a>
                 </div>
             </div>
-            <img className="profile-image mb-3 mb-lg-0 ml-lg-5 mr-md-0" src={Image} alt=""/>
+            <img className="profile-image mb-3 mb-lg-0 ml-lg-5 mr-md-0" src={Image} alt="Image of Tashan"/>
         </div>
     </div>
 </section>
