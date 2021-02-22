0e35a8e275a1ee8a308c7f1f37c1eab84e8ebe37
viewtifult <tashanduncan1994@gmail.com>
2021-02-22 22:53:55 +0000

    edit skills section

--- my-app/src/components/Main/Skills.js
+++ my-app/src/components/Main/Skills.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faJsSquare, faReact, faNodeJs, faPhp, faSass, faNpm, faHtml5, faCss3Alt, faWordpressSimple, faLaravel, faVuejs} from '@fortawesome/free-brands-svg-icons'
+import { faJsSquare, faReact, faPhp, faSass, faHtml5, faCss3Alt, faWordpressSimple, faLaravel,} from '@fortawesome/free-brands-svg-icons'
+import {faDatabase } from '@fortawesome/free-solid-svg-icons'
 
 const Skills = () => (
     <section class="overview-section p-3 p-lg-5">
@@ -44,8 +45,8 @@ const Skills = () => (
             
             <div class="item col-6 col-lg-3">
                 <div class="item-inner">
-                    <div class="item-icon"><FontAwesomeIcon icon={faVuejs}/></div>
-                    <h3 class="item-title">Vue</h3>
+                    <div class="item-icon"><FontAwesomeIcon className="mr-2" icon={faDatabase}/></div>
+                    <h3 class="item-title">SQL</h3>
                     <div class="item-desc">List skills/technologies here. You can change the icon above to any of the 1500+ <a class="theme-link" href="https://fontawesome.com/" target="_blank">FontAwesome 5 free icons</a> available. Aenean commodo ligula eget dolor.  </div>
                 </div>
             </div>
